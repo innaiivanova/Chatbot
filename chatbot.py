92dfc7472934a6d417d3854ab962a8c067ebee04
Inna Ivanova Ivanova <innaiivanova@gmail.com>
2021-11-04 14:00:19 +0100

    fix: update version from 1 to 1.0

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 # Building first chatbot
 # 03.11.2021
 
-print("Welcome! I'm chatbot version 1.")
+print("Welcome! I'm chatbot version 1.0")
 print("I can repeat what you say.")
 stuff_to_echo = input("What do you want me to repeat?")
 print("You said: "+ stuff_to_echo)
